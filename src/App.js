b24cb4fec548031f1af98d92c0972d2c624654b2
Dastinss <sergeyfdaragan@gmail.com>
2023-02-27 02:26:14 +0200

    #42 done with error 'subscriber'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import state from "./redux/store";
+import store from "./redux/store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ( props ) => {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path="/dialogs"
+                               element={<Dialogs store={props.store}/>}/>
+                        <Route path="/profile"
+                               element={<Profile
+                                   profilePage={props.state.profilePage}
+                                   // addPost={props.addPost}
+                                   // updateNewPostText={props.updateNewPostText}/>}/>
+
+                                   //заменили закоменченные выше два метода на один dispatch
+                                   dispatch={props.dispatch}/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>)
 }
 
 export default App;
